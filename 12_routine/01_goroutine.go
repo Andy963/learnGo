deaba8f0c960a9ee1e7af2dc1cf4feaa32ce67d9
Andy963 <zjgxs518@126.com>
2020-09-28 08:11:31 +0800

    go routine

---
+++ 12_routine/01_goroutine.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func Task() {
+	for {
+		fmt.Println("this is new task")
+		time.Sleep(time.Second)
+	}
+}
+func main() {
+
+	go Task() // 新建一个协程
+	for {
+		fmt.Println("this is main task")
+		time.Sleep(time.Second)
+	}
+}
