ece352936e2a6214d712223b51d038e2a70b2fb7
Andy963 <zjgxs518@126.com>
2020-10-07 13:51:50 +0800

    go tcp socket cs

---
+++ 13_socket/01_tcp_server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "127.0.0.1:8000")
+	if err != nil {
+		fmt.Println("err= ", err)
+		return
+	}
+	defer listener.Close()
+
+	// block, to wait  for connect
+	conn, err1 := listener.Accept()
+	if err1 != nil {
+		fmt.Println("err1= ", err1)
+		return
+	}
+
+	// receive request
+	buf := make([]byte, 1024)
+	n, err2 := conn.Read(buf)
+	if err2 != nil {
+		fmt.Println("err2= ", err2)
+		return
+	}
+	fmt.Println("buf= ", string(buf[:n]))
+
+	defer conn.Close() // close connection
+	fmt.Printf("")
+}
