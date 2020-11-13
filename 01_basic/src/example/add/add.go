f892fc1419f83a96a4d7494626ff7a340bd48159
Andy963 <zjgxs518@126.com>
2020-11-14 07:26:18 +0800

    包的初始化，引用

---
+++ 01_basic/src/example/add/add.go
@@ -0,0 +1,10 @@
+package add
+
+
+var Name string = "hello world"
+var Age int = 10
+
+func init() {
+	Name = "I'm Name"
+	Age = 29
+}
---
+++ 01_basic/src/example/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	packageAdd "example/add"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Name=", packageAdd.Name)
+	fmt.Println("Age=", packageAdd.Age)
+}
